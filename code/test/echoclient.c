e6540b93ea400244fdb7164cf9999983de67a3ab
DawnOfJune <110721185+lhson0606@users.noreply.github.com>
2023-11-06 23:27:20 +0700

    Revert "Initiation for writing test"

--- code/test/echoclient.c
+++
@@ -1,9 +0,0 @@
-#include "syscall.h"
-#include "copyright.h"
-
-int main(){
-    //write echoclient program here
-    
-
-    Halt();
-}
--- code/threads/kernel.h
+++ code/threads/kernel.h
@@ -2,17 +2,11 @@
 //	Global variables for the Nachos kernel.
 //
 // Copyright (c) 1992-1996 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation
+// All rights reserved.  See copyright.h for copyright notice and limitation 
 // of liability and disclaimer of warranty provisions.
 
 #ifndef KERNEL_H
 #define KERNEL_H
-
-#define __USER_ARGS__
-
-#ifdef __USER_ARGS__
-#define MAX_ARG_NUM 10
-#endif
 
 #include "copyright.h"
 #include "debug.h"
@@ -31,55 +25,52 @@ class SynchConsoleInput;
 class SynchConsoleOutput;
 class SynchDisk;
 
-class Kernel
-{
-public:
-  Kernel(int argc, char **argv);
-  // Interpret command line arguments
-  ~Kernel(); // deallocate the kernel
+class Kernel {
+  public:
+    Kernel(int argc, char **argv);
+    				// Interpret command line arguments
+    ~Kernel();		        // deallocate the kernel
+    
+    void Initialize(); 		// initialize the kernel -- separated
+				// from constructor because 
+				// refers to "kernel" as a global
 
-  void Initialize(); // initialize the kernel -- separated
-                     // from constructor because
-                     // refers to "kernel" as a global
+    void ThreadSelfTest();	// self test of threads and synchronization
 
-  void ThreadSelfTest(); // self test of threads and synchronization
+    void ConsoleTest();         // interactive console self test
 
-  void ConsoleTest(); // interactive console self test
+    void NetworkTest();         // interactive 2-machine network test
+    
+// These are public for notational convenience; really, 
+// they're global variables used everywhere.
 
-  void NetworkTest(); // interactive 2-machine network test
+    Thread *currentThread;	// the thread holding the CPU
+    Scheduler *scheduler;	// the ready list
+    Interrupt *interrupt;	// interrupt status
+    Statistics *stats;		// performance metrics
+    Alarm *alarm;		// the software alarm clock    
+    Machine *machine;           // the simulated CPU
+    SynchConsoleInput *synchConsoleIn;
+    SynchConsoleOutput *synchConsoleOut;
+    SynchDisk *synchDisk;
+    FileSystem *fileSystem;     
+    PostOfficeInput *postOfficeIn;
+    PostOfficeOutput *postOfficeOut;
 
-  // These are public for notational convenience; really,
-  // they're global variables used everywhere.
+    int hostName;               // machine identifier
 
-  Thread *currentThread; // the thread holding the CPU
-  Scheduler *scheduler;  // the ready list
-  Interrupt *interrupt;  // interrupt status
-  Statistics *stats;     // performance metrics
-  Alarm *alarm;          // the software alarm clock
-  Machine *machine;      // the simulated CPU
-  SynchConsoleInput *synchConsoleIn;
-  SynchConsoleOutput *synchConsoleOut;
-  SynchDisk *synchDisk;
-  FileSystem *fileSystem;
-  PostOfficeInput *postOfficeIn;
-  PostOfficeOutput *postOfficeOut;
-
-  int hostName; // machine identifier
-  
-#ifdef __USER_ARGS__
-  char *userArgs[MAX_ARG_NUM];
-  int userArgc = 0;
-#endif
-
-private:
-  bool randomSlice;   // enable pseudo-random time slicing
-  bool debugUserProg; // single step user program
-  double reliability; // likelihood messages are dropped
-  char *consoleIn;    // file to read console input from
-  char *consoleOut;   // file to send console output to
+  private:
+    bool randomSlice;		// enable pseudo-random time slicing
+    bool debugUserProg;         // single step user program
+    double reliability;         // likelihood messages are dropped
+    char *consoleIn;            // file to read console input from
+    char *consoleOut;           // file to send console output to
 #ifndef FILESYS_STUB
-  bool formatFlag; // format the disk if this is true
+    bool formatFlag;          // format the disk if this is true
 #endif
 };
 
+
 #endif // KERNEL_H
+
+
